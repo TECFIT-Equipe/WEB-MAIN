b9d60dcd391404bdab0bd35774cf94ac85aa5a5c
ADalosto <andreydalosto5555@gmail.com>
2024-09-19 17:44:06 -0300

    criado um arquivo footer

---
+++ parts/footer.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500;700&family=Rubik:wght@400;500;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+
+<body>
+    <footer class="footer"
+        style="background-image: url('https://raw.githubusercontent.com/farazc60/Project-Images/main/medical/assets/footer-bg.png')">
+        <div class="container">
+            <div class="section footer-top">
+                <div class="footer-brand" data-reveal="bottom">
+                    <a href="#" class="logo">
+                        <img src="../images/logosemfundo.png" width="70" height="30" loading="lazy" alt="Doclab home">
+                        <br>
+                        <img src="../images/logocotilazul.png" width="70" height="30" loading="lazy" alt="Doclab home">
+                    </a>
+                    <ul class="contact-list has-after">
+                        <li class="contact-item">
+                            <div class="item-icon">
+                                <ion-icon name="mail-open-outline"></ion-icon>
+                            </div>
+                            <div>
+                                <p>
+                                    Email : <a href="mailto:contact@website.com"
+                                        class="contact-link">contato@tecfit.com</a>
+                                </p>
+                            </div>
+                        </li>
+                        <li class="contact-item">
+                            <div class="item-icon">
+                                <ion-icon name="call-outline"></ion-icon>
+                            </div>
+                            <div>
+                                <p>
+                                    Telefone : <a href="tel:66666666666666" class="contact-link">66666666666666</a>
+                                </p>
+                                <p>
+                                    Celular : <a href="tel:66666666666666" class="contact-link">66666666666666</a>
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-list" data-reveal="bottom">
+                    <p class="headline-sm footer-list-title">Sobre Nós</p>
+                    <p class="text">
+                        Desenvolvido Por Alunos do Cotil<br>
+                        Sob Orientação Do Prof. Danilo
+                    </p>
+                    <address class="address">
+                        <ion-icon name="map-outline"></ion-icon>
+                        <span class="text">
+                            R. Paschoal Marmo, 1888 - Jardim Nova Italia<br>
+                            Limeira - SP, 13484-431
+                        </span>
+                    </address>
+                </div>
+                <ul class="footer-list" data-reveal="bottom">
+                    <li>
+                        <p class="headline-sm footer-list-title">Serviços</p>
+                    </li>
+                    <li>
+                        <a href="#" class="text footer-link">Como Funciona</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text footer-link">O Que Oferecemos</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text footer-link">Últimos Artigos & Notícias</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text footer-link">Nós Contate</a>
+                    </li>
+                </ul>
+                <ul class="footer-list" data-reveal="bottom">
+                    <li>
+                        <p class="headline-sm footer-list-title">Links Úteis</p>
+                    </li>
+                    <li>
+                        <a href="#" class="text footer-link">Termos de Uso</a>
+                    </li>
+
+                </ul>
+                <div class="footer-list" data-reveal="bottom">
+                    <p class="headline-sm footer-list-title">Inscreva-se</p>
+
+                    <form action="" class="footer-form">
+                        <input type="email" name="email" placeholder="Email" class="input-field title-lg">
+
+                        <button type="submit" class="btn has-before title-md">Inscreva-se</button>
+                    </form>
+                    <p class="text">
+                        Receba as últimas notícias via email. A qualquer momento você pode cancelar a inscrição
+                    </p>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p class="text copyright">
+                    Ⓡ TecFit 2024 | Todos os direitos reservados à TecFit
+                </p>
+                <ul class="social-list">
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-facebook"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-twitter"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-google"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="social-link">
+                            <ion-icon name="logo-pinterest"></ion-icon>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <a href="#top" class="back-top-btn" aria-label="back to top" data-back-top-btn>
+        <ion-icon name="chevron-up"></ion-icon>
+    </a>
+    <script src="../js/script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
